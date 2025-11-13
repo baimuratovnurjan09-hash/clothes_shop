a684a62640a0e1f2510ed99f91fef78f7dbc664c
Dawlet04 <saymanovaguliy@gmail.com>
2025-11-13 11:07:58 +0500

    views.py

--- clothes/views.py
+++ clothes/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render
 from .models import Clothes
 
 def homepage(request):
-  clothes = Clothes.objects.all()
-  context_menu = {
-    'clothes':clothes
-  }
-  return render(request,'home.html',context_menu)
+    query = request.GET.get('q')  # Получаем текст из поискового поля
+    if query:
+        clothes = Clothes.objects.filter(title__icontains=query)
+    else:
+        clothes = Clothes.objects.all()
 
+    context_menu = {
+        'clothes': clothes,
+        'query': query,
+    }
+    return render(request, 'home.html', context_menu)
 # Create your views here.
