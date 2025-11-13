89b86fb42ade6273dc305549c1f88b4cba9e97ea
Dawlet04 <saymanovaguliy@gmail.com>
2025-11-13 10:40:25 +0500

    views.py

--- clothes/views.py
+++ clothes/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Clothes
 
 def homepage(request):
-  return render(request,'home.html')
+  clothes = Clothes.objects.all()
+  context_menu = {
+    'clothes':clothes
+  }
+  return render(request,'home.html',context_menu)
 
 # Create your views here.
